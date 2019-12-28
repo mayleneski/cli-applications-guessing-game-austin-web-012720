29700a42caaebd82900706b59d8134e3b6e944f0
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-28 19:00:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
  puts "Guess a number between 1 and 6"
  input = gets.chomp
  
- if input == number
+ if input.to_i == number
    puts "You guessed the correct number!"
  elsif input != number
    puts "Sorry! The computer guessed #{number}."
